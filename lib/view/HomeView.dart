69ee35860e10e6874f885940372db2152ad4ef1f
Dev-Aegis <dev.davidlim@gmail.com>
2024-10-03 15:14:49 +0900

    Example Electronic Picture Frame

--- lib/view/HomeView.dart
+++ lib/view/HomeView.dart
@@ -1,31 +1,53 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
 
-class HomeView extends StatelessWidget {
-  WebViewController webViewController = WebViewController()
-  ..loadRequest(Uri.parse('https://blog.codefactory.ai'))
-  ..setJavaScriptMode(JavaScriptMode.unrestricted);
-
-  HomeView({Key? key}) : super(key: key);
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.redAccent,
-        title: Text("Studio Red"),
-        centerTitle: true,
+  State<HomeView> createState() => _HomeViewState();
+}
 
-        actions: [
-          IconButton(onPressed: () {
-            webViewController.loadRequest((Uri.parse('https://blog.codefactory.ai')));
-          }, icon: const Icon(
-            Icons.home
-          ))
-        ],
-      ),
-      body: WebViewWidget(
-        controller: webViewController,
+class _HomeViewState extends State<HomeView> {
+  List<Image> imgList = [1, 2, 3, 4, 5].map((number) => Image.asset('assets/img/page_$number.jpeg', fit: BoxFit.cover,)).toList();
+  final PageController pageController = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+    
+    Timer.periodic(
+      Duration(seconds: 3), (timer) {
+        print("실행!");
+        int? nextPage = pageController.page?.toInt();
+
+        if(nextPage == null) return;
+
+        if(nextPage == 4) {
+          nextPage = 0;
+        } else {
+          ++nextPage;
+        }
+
+        pageController.animateToPage(
+            nextPage,
+            duration: Duration(milliseconds: 500),
+            curve: Curves.ease);
+      },
+    );
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+
+    return Scaffold(
+      body: PageView(
+        controller: pageController,
+        children: imgList,
       )
     );
   }
